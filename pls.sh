899aa98dcc779438f21a65dd021ca21922e1fd3c
uncreativeCultist <66176496+uncreativeCultist@users.noreply.github.com>
2026-04-22 23:04:39 -0400

    added "pls update self" and "pls help"

--- pls.sh
+++ pls.sh
@@ -1,56 +1,83 @@
 #!/bin/bash
+PLS_VER=042226a
+echo pls - $PLS_VER
+echo -------------------
 
 ## please update ----------------------------------
 if [[ "$1" == "update" ]]; then
-  if command -v apt &>/dev/null # apt handling - debian
-  then
-    echo "updating apt packages"
-    sudo apt update
-    sudo apt upgrade
-  elif command -v apt-get &>/dev/null # apt handling - debian
-  then
-    echo "updating apt packages"
-    sudo apt-get update
-    sudo apt-get upgrade
-  fi
 
-  if command -v pacman &>/dev/null # pacman handling - arch
-  then
-    echo "updating pacman packages"
-    sudo pacman -Syu
-  fi
+  if [[ "$2" == "self" ]]; then
+    echo pretend this is self updating
+    # Source - https://stackoverflow.com/a/246128
+    # Posted by dogbane, modified by community. See post 'Timeline' for change history
+    # Retrieved 2026-04-22, License - CC BY-SA 4.0
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-  if command -v flatpak &>/dev/null # flatpak handling
-  then
-    echo "updating flatpaks"
-    sudo flatpak update
-  fi
+    echo $SCRIPT_DIR
 
-  if command -v chwd &>/dev/null # chwd handling - cachyos
-  then
-    #echo "ishowdrivers: lets update!" #what the fuck was i thinking
-    echo "updating drivers"
-    sudo chwd -a
-  fi
+    if command -v wget &>/dev/null # yo do we have wget
+    then
+      wget "https://raw.githubusercontent.com/uncreativeCultist/pls/refs/heads/main/pls.sh" -P $SCRIPT_DIR 
+      echo "pls has been updated!"
+    else
+      echo "update self requires wget. please install wget!!"
+    fi
 
-  if command -v yay &>/dev/null # AUR handling - arch - yay
-  then
-    read -p "do you wanna update AUR stuff? this could take a while. (y/n): " -n 1 -r
+  else
+    if command -v apt &>/dev/null # apt handling - debian
+    then
+      echo "updating apt packages"
+      sudo apt update
+      sudo apt upgrade
+    elif command -v apt-get &>/dev/null # apt handling - debian
+    then
+      echo "updating apt packages"
+      sudo apt-get update
+      sudo apt-get upgrade
+    fi
+
+    if command -v pacman &>/dev/null # pacman handling - arch
+    then
+      echo "updating pacman packages"
+      sudo pacman -Syu
+    fi
+
+    if command -v flatpak &>/dev/null # flatpak handling
+    then
+      echo "updating flatpaks"
+      sudo flatpak update
+    fi
+
+    if command -v chwd &>/dev/null # chwd handling - cachyos
+    then
+      #echo "ishowdrivers: lets update!" #what the fuck was i thinking
+      echo "updating drivers"
+      sudo chwd -a
+    fi
+
+    if command -v yay &>/dev/null # AUR handling - arch - yay
+    then
+      read -p "do you wanna update AUR stuff? this could take a while. (y/n): " -n 1 -r
+      echo   
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+          yay -Syu
+      fi
+    fi
+
+    read -p "do you wanna reboot? (you probably should tbh) (y/n): " -n 1 -r
     echo   
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        yay -Syu
+        sudo reboot
+      fi
     fi
-  fi
-
-  read -p "do you wanna reboot? (you probably should tbh) (y/n): " -n 1 -r
-  echo   
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-      sudo reboot
-  fi ## please update END----------------------------------
-elif [[ "$1" == "install" ]]; then
-  echo pretend this is installing/updating
+       ## please update END----------------------------------
+elif [[ "$1" == "help" ]]; then
+  echo pls help - shows this list
+  echo "pls update - updates as much as possible (pls not included)"
+  echo pls update self - updates pls
 else
-  echo "i dunno wut 2 do?"
+  echo "'pls $1' is not a recognized command, sorry :("
+  echo 'maybe try "pls help" to see possible arguments?'
 fi
